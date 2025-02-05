81b4657c8ce68f8b703e17cfac3bae18d2307e9e
Hamza abduljalil <hamza.abduljalil100@gmail.com>
2025-02-06 01:10:22 +0300

    Added new files3

--- src/components/Portfolio/data.js
+++ src/components/Portfolio/data.js
@@ -9,6 +9,14 @@ import expensesapp1 from "../../assets/project/expensesqpp/page1.png";
 import expensesapp2 from "../../assets/project/expensesqpp/page2.png";
 import ReactMeals1 from "../../assets/project/ReactMeals/page1.png";
 import ReactMeals2 from "../../assets/project/ReactMeals/page2.png";
+import guessmynumber1 from "../../assets/project/guessmynumber/page1.png";
+import guessmynumber2 from "../../assets/project/guessmynumber/page2.png";
+import guessmynumber3 from "../../assets/project/guessmynumber/page3.png";
+import bank1 from "../../assets/project/bank/page1.png";
+import piggame1 from "../../assets/project/piggame/page1.png";
+import piggame2 from "../../assets/project/piggame/page2.png";
+import mapty1 from "../../assets/project/Mapty/page1.png";
+import mapty2 from "../../assets/project/Mapty/page2.png";
 import img1 from "../../assets/project/page1.png";
 
 export const data = [
@@ -83,5 +91,49 @@ export const data = [
       "ReactMeals is a sleek and responsive food ordering web application built using React. It provides users with a seamless way to browse and order delicious meals from a carefully curated menu.The homepage features a visually appealing hero section with a welcoming message, inviting users to choose from a variety of high-quality dishes prepared by experienced chefs. Each meal listing includes a brief description, price, and an easy-to-use quantity selector with an “Add” button for effortless order placement.A shopping cart system is integrated into the interface, allowing users to track their selected items and proceed to checkout smoothly. With a modern design and intuitive navigation, ReactMeals enhances the online food ordering experience, making it convenient and enjoyable for users.",
     github: "https://github.com/hamzaabduljalil/ReactMeals",
     hosting: "https://hamzaabduljalil.github.io/ReactMeals/",
+  },
+  {
+    id: 7,
+    title: "Guess-My-Number",
+    summary: "FrontEnd web application developed using JavaScript",
+    Language: ["JavaScript", "WebDesign"],
+    img: [guessmynumber1, guessmynumber2, guessmynumber3],
+    Description:
+      'This fun and interactive web-based game challenges players to guess a randomly generated number between 1 and 20 within only five attempts. Built using JavaScript, the game offers a simple yet engaging experience with real-time feedback to guide players toward the correct answer.Players enter their guesses, and the game provides hints such as "Too High" or "Too Low" to help them refine their choices. A limited number of tries adds an element of excitement and strategy. The interface is designed to be user-friendly and responsive, ensuring smooth gameplay across different devices.With its minimalist design and engaging logic, the Guess-My-Number game is a great way to test and improve number-guessing skills while having fun!',
+    github: "https://github.com/hamzaabduljalil/Guess-My-Number",
+    hosting: "https://hamzaabduljalil.github.io/Guess-My-Number/",
+  },
+  {
+    id: 8,
+    title: "pig-game",
+    summary: "FrontEnd web application developed using JavaScript",
+    Language: ["JavaScript", "WebDesign"],
+    img: [piggame1, piggame2],
+    Description:
+      'Pig Game is a two-player interactive game where participants roll a dice and accumulate points. The objective is to reach 100 points before the opponent. However, rolling a "1" resets the player\'s turn score to zero. Players must strategically decide whether to "Hold" their score or take a risk and continue rolling. The game, built with JavaScript, features a clean and engaging UI, making it an exciting experience for all ages.',
+    github: "https://github.com/hamzaabduljalil/Pig-Game",
+    hosting: "https://hamzaabduljalil.github.io/Pig-Game/",
+  },
+  {
+    id: 9,
+    title: "Mapty-app",
+    summary: "FrontEnd web application developed using JavaScript",
+    Language: ["JavaScript", "WebDesign"],
+    img: [mapty1, mapty2],
+    Description:
+      "Mapty is a web application that allows users to track and log their running activities with an interactive map. Built using JavaScript and Leaflet, this application provides a seamless way for users to record and analyze their workouts.The platform enables users to select and mark their running locations, log workout details such as distance, duration, pace, and steps per minute, and visualize their progress over time. With an intuitive and modern interface, Mapty makes fitness tracking simple and engaging.Designed for ease of use and accessibility, Mapty features a clean layout and a dark-themed dashboard for a smooth user experience. Whether you're a casual jogger or a dedicated runner, this application helps you stay motivated and organized in your fitness journey.",
+    github: "https://github.com/hamzaabduljalil/Mapty",
+    hosting: "https://hamzaabduljalil.github.io/Mapty/",
+  },
+  {
+    id: 10,
+    title: "bank-app",
+    summary: "FrontEnd web application developed using JavaScript",
+    Language: ["JavaScript", "WebDesign"],
+    img: [bank1],
+    Description:
+      "his modern and user-friendly banking web application enables users to manage their finances efficiently. Built with a sleek and intuitive design, the platform allows users to perform essential banking operations such as transferring money and requesting loans with ease.The homepage provides a clear overview of the user’s current balance, recent transactions, and financial insights. Transactions, including deposits and withdrawals, are displayed in an organized manner, giving users a transparent view of their account activity.Key features include:Money Transfers: Users can send money to other accounts quickly by entering the recipient’s details and the transfer amountLoan Requests: A simple loan request system allows users to apply for financial support instantlyAccount Management: Users can monitor their income, expenses, and interest calculations in real-timeWith a modern UI and seamless functionality, this banking web application enhances financial management while ensuring a smooth and secure user experience.",
+    github: "https://github.com/hamzaabduljalil/Bank",
+    hosting: "https://hamzaabduljalil.github.io/Bank/",
   },
 ];
