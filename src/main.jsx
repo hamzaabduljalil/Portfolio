86693558ed9960c6ad67bbff806611ac07f3982f
hamzaabduljalil <128858715+hamzaabduljalil@users.noreply.github.com>
2025-02-06 00:07:31 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App"; // Ensure the file path is correct
 import "./index.css"; // Optional CSS import
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Portfolio/" >
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
